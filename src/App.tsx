2c4b8dfa91167887f8754a0da3b7ae39cf00b03f
FuuKowatty <miedzio.2000@gmail.com>
2023-01-28 14:21:52 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/search' element={<Search />}/>
+        <Route path='/watch' element={<Watch />}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
