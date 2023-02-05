53af05139a66b9b2073ab9491209f82ee125221f
FuuKowatty <miedzio.2000@gmail.com>
2023-02-05 10:28:09 +0100

    new libs, ts types

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -115,7 +115,7 @@ export default function Sidebar() {
   ];
 
   return (
-    <div className="w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 sidebar">
+    <div className="w-2/12 min-w-[300px] bg-[#212121] pr-5 overflow-auto pb-8 sidebar">
       <ul className="flex flex-col border-b-2 border-gray-700">
         {mainLinks.map(({ icon, name }) => {
           return (
